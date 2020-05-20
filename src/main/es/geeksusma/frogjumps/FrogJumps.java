d60e0fd6e736e316b76e167b4a31894faeaea800
geeksusma <jesus.maria.villar.vazquez@gmail.com>
2020-05-20 18:24:26 +0200

    added frog jumps

---
+++ src/main/es/geeksusma/frogjumps/FrogJumps.java
@@ -0,0 +1,11 @@
+package geeksusma.frogjumps;
+
+public class FrogJumps {
+
+    public int solution(int start, int end, int jumpSize) {
+        // write your code in Java SE 8
+        int totalPositions = end - start;
+
+        return (int) Math.ceil(new Double(totalPositions) / new Double(jumpSize));
+    }
+}
---
+++ src/test/es/geeksusma/frogjumps/FrogJumpsTest.java
@@ -0,0 +1,30 @@
+package geeksusma.frogjumps;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(JUnit4.class)
+public class FrogJumpsTest {
+
+    private FrogJumps frogJumps;
+
+    @Before
+    public void setUp() {
+        frogJumps = new FrogJumps();
+    }
+
+    @Test
+    public void should_calculateJumps_when_frogJumps() {
+
+        //when
+        final int solution = frogJumps.solution(10, 80, 30);
+
+        //then
+        assertThat(solution).isEqualTo(3);
+    }
+}
